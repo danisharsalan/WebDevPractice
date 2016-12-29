68a40c541a5c139ff2cb4f210eb124b1381e3a35
danisharsalan <danisharsalan@yahoo.com>
2016-12-29 01:49:30 -0500

    practicing array manipulation

--- list.js
+++ list.js
@@ -2,13 +2,39 @@ var todos = [];
 
 var input = prompt("What would you like to do?");
 
-while(input !== "quit"){
+while(input !== "quit") {
 	if(input === "list") {
-	console.log(todos);
+		listTodos();
 	} else if(input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		addTodo();
+	} else if(input === "delete") {
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
+
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos(){
+	console.log("**********");
+	if(todos.length === 0){
+		console.log("You have nothing to do!");
+	} else {
+		todos.forEach(function(todo, index){
+			console.log(index + ": " + todo);
+		});
+	}
+	console.log("**********");
+}
+
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
