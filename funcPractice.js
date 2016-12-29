b0e52977456248c83a31ce72c3d4d13a4219a7c1
danisharsalan <danisharsalan@yahoo.com>
2016-12-29 00:26:40 -0500

    practicing functions

---
+++ funcPractice.js
@@ -0,0 +1,29 @@
+console.log(isEven(2));
+console.log(isEven(3));
+function isEven(num) {
+	if(num % 2 === 0){
+		return true;
+	}
+	return false;
+}
+
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(3));
+function factorial(num) {
+	if(num === 0){
+		return 1;
+	}
+	var answer = 1;
+	for(var x = 1; x <= num; x++){
+		answer *= x;
+	}
+	return answer;
+}
+
+console.log(kebabToSnake("dogs-are-awesome"));
+console.log(kebabToSnake("hello-world"));
+console.log(kebabToSnake("blah"));
+function kebabToSnake(input){
+	return input.replace(/-/g, "_");
+}
