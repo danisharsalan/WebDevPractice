e6c35a4853d6260ef17bb5b3a3ca753bddb5d0c5
danisharsalan <danisharsalan@yahoo.com>
2017-01-07 21:02:18 -0500

    Practice with EJS and GET/POST Requests

--- MoreExpress/EJSPractice/app.js
+++ MoreExpress/EJSPractice/app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-	res.render("home.ejs");
+	res.render("home");
 });
 
 // "/fallinlovewith/dogs" has html saying "You have fallen in love with dogs"
 app.get("/fallinlovewith/:thing", function(req, res){
 	var thing = req.params.thing;
-	res.render("love.ejs", {thingVar: thing});
+	res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -17,8 +20,7 @@ app.get("/posts", function(req, res){
 		{title: "My adorable pet bunny", author: "Charlie"},
 		{title: "Can you believe this?!?!", author: "Buzzfeed"}
 	];
-
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
 });
 
 app.listen(3000, function(){
